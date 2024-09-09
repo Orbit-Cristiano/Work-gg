155d777af455e5fa763cf25fdf87394aa645aa94
Orbit-Cristiano <148749659+Orbit-Cristiano@users.noreply.github.com>
2024-09-09 13:52:16 +0700

    Add files via upload

---
+++ f/sdfs.py
@@ -0,0 +1,48 @@
+
+
+# def reccurent_find_value(tests: dict, test_id: int, value_set):
+#     # ищем тест без value
+#     for test in tests:
+#         if test['id'] is test_id:
+#             test['value'] = value_set
+#             return
+#         if 'values' in test:
+#             reccurent_find_value(test['values'], test_id, value_set)
+#     return
+
+
+# for item in values_json['values']:
+#     value_test_id = item['id']
+#     value = reccurent_find_value(tests_json['tests'], value_test_id, item['value'])
+
+
+# with open('report.json', 'w') as f:
+#     print(json.dump(tests_json, f, indent=4))
+    
+    
+import json
+from collections import defaultdict
+
+with open(r"C:\Users\User\Desktop\f\tests.json") as f:
+    tests_json = json.load(f)
+    
+with open(r"C:\Users\User\Desktop\f\values.json") as f:
+    values_json = json.load(f)
+def reccurent_find_value(tests: dict, test_id: int, value_set):
+    for test in tests:
+        if test['id'] == test_id:
+            test['value'] = value_set
+            return
+        if 'values' in test:
+            if reccurent_find_value(test['values'], test_id, value_set):
+                return True
+    return False
+    
+    
+    for item in values_json['values']:
+        value_test_id = item['id']
+        if reccurent_find_value(tests_json['tests'], value_test_id, item['value']):
+            print(f"Value updated for test with id {value_test_id}")
+
+with open('report.json', 'w') as f:
+    json.dump(tests_json, f, indent=4)
